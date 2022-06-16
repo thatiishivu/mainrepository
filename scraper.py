f3b04c3bd74b22f7a71a77d9559e36794b97aac8
shivuLT <58023814+thatiishivu@users.noreply.github.com>
2022-06-16 13:07:07 +0000

    updated code

--- scraper.py
+++ scraper.py
@@ -1,1 +1,12 @@
-print('hello world')
+import requests
+from bs4 import BeautifulSoup
+print('i love racchy')
+response_out = requests.get('https://www.youtube.com/watch?v=FcW-AXsirBE')
+print('connection status:',response_out.status_code)
+with open('trending.html', 'w') as f:
+  f.write(response_out.text)
+
+doc = BeautifulSoup(response_out.text,'html.parser')
+
+print('title of the document',doc.title.text)
+print('racchu loves me')
