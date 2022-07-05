94d32cb91062b802632d52e5e2fa871c38dff89e
shivuLT <58023814+thatiishivu@users.noreply.github.com>
2022-07-05 08:35:34 +0000

    added driver and there packages

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 #https://www.youtube.com/watch?v=FcW-AXsirBE
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 print('i love racchy')
 response_out = requests.get('https://www.youtube.com/watch?v=FcW-AXsirBE')
 print('connection status:',response_out.status_code)
@@ -11,3 +14,11 @@ doc = BeautifulSoup(response_out.text,'html.parser')
 
 print('title of the document',doc.title.text)
 print('racchu loves me')
+
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=chrome_options)
+urltohit_onreplit = 'https://www.youtube.com/watch?v=FcW-AXsirBE'
+driver.get(urltohit_onreplit)
+
